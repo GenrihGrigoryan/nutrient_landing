45b9bd97e01aa12583647d5096cb95584f11423b
Genrih Grigoryan <grigoryang1995@yandex.ru>
2025-06-12 18:50:49 +0500

    upd

---
+++ _app/immutable/chunks/dtYb8TA-.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ezqddj)==null?void 0:s.base)??"/nutrient_landing";var e;const a=((e=globalThis.__sveltekit_ezqddj)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/chunks/uXPmO5ZN.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1ejyltj)==null?void 0:s.base)??"/nutrient_landing";var t;const a=((t=globalThis.__sveltekit_1ejyltj)==null?void 0:t.assets)??e;export{a,e as b};
