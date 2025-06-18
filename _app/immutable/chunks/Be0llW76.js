aff79538e93ea94fa49142928def2a613317f7da
Genrih Grigoryan <grigoryang1995@yandex.ru>
2025-06-18 21:10:42 +0500

    new

--- _app/immutable/chunks/Be0llW76.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./I4Rntq72.js";a();
--- _app/immutable/nodes/0.ChOpW1wf.js
+++
@@ -1,1 +0,0 @@
-import{c as a,a as n}from"../chunks/CPEmkoFv.js";import"../chunks/Be0llW76.js";import{q as m}from"../chunks/I4Rntq72.js";import{s}from"../chunks/8zPoaLsp.js";const l=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:l},Symbol.toStringTag,{value:"Module"}));function d(t,e){var o=a(),r=m(o);s(r,e,"default",{}),n(t,o)}export{d as component,u as universal};
